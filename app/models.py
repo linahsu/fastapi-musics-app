0cdef144ffa38f2a599e04bcfe9d9bd522aa8f1f
Lina Hsu <linahthsu@gmail.com>
2024-07-16 17:08:35 -0300

    feat: implements class models Song and SongInDb

--- app/models.py
+++ app/models.py
@@ -1,1 +1,16 @@
-# Aqui será a definição dos modelos de dados
+from pydantic import BaseModel, Field
+from app.database import db
+
+class Song(BaseModel):
+  name: str
+  artist: str
+  album: str
+  release_year: int
+  genre: str
+  image: str
+  
+class SongInDb(Song):
+  id: str = Field(alias="_id")
+  
+class SongLibrary:
+  _collection = db["songs"]
