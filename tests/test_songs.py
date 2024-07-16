c084fa476e8c22c9b8a05a14bf1f1fa15e53d38e
Lina Hsu <linahthsu@gmail.com>
2024-07-16 18:31:58 -0300

    feat: implements test for get_all_songs endpoint

--- tests/test_songs.py
+++ tests/test_songs.py
@@ -16,14 +16,33 @@ def client():
 
 
 def test_add_song(client):
-  song = {
+  song1 = {
     "name": "Song 1",
     "artist": "Artist 1",
     "album": "Album 1",
     "release_year": 2021,
     "genre": "Pop",
-    "image": "https://example.com/image.jpg"
+    "image": "https://example.com/image1.jpg"
   }
-  response = client.post("/songs", json=song)
+  
+  response = client.post("/songs", json=song1)
+  
   assert response.status_code == 201
   assert response.json() == "Song added successfully"
+  
+def test_get_all_songs(client):
+    song2 = {
+    "name": "Song 2",
+    "artist": "Artist 2",
+    "album": "Album 2",
+    "release_year": 2012,
+    "genre": "Rock",
+    "image": "https://example.com/image2.jpg"
+  }
+    client.post("/songs", json=song2)
+    
+    response = client.get("/songs")
+    
+    assert response.len == 2
+    assert response[0].name == "Song 1"
+    assert response[1].name == "Song 2"
